1ca37eef23a8c67cf3a102ef17219b87d3d83a71
roconnor <roconnor@r6.ca>
2023-04-28 23:54:01 -0400

    Rename tcc-pass1 to tcc-mes

--- default.nix
+++ default.nix
@@ -12,19 +12,19 @@ in lib.fix
   stage0-seed = callPackage ./stage0/seed.nix {};
   mes = callPackage ./mes-0.24.2 {};
   "tcc-0.9.26" = callPackage ./tinycc/0.9.26.nix {};
-  tcc-pass1 = callPackage ./tinycc/0.9.27-pass1.nix { CC = "${final."tcc-0.9.26"}/bin/tcc"; };
-  libgetopt = callPackage ./libgetopt { tcc = tcc-pass1; };
-  make = callPackage ./make-3.82 { tcc = tcc-pass1; };
-  gzip = callPackage ./gzip-1.2.4 { tcc = tcc-pass1; };
-  tar = callPackage ./tar-1.12 { tcc = tcc-pass1; };
-  bzip2 = callPackage ./bzip2-1.0.8 { tcc = tcc-pass1; };
-  sed = callPackage ./sed-4.0.9 { tcc = tcc-pass1; };
-  patch = callPackage ./patch-2.5.9 { tcc = tcc-pass1; };
-  coreutils = callPackage ./coreutils-5.0 { tcc = tcc-pass1; };
-  heirloom-devtools = callPackage ./heirloom-devtools-070527 { tcc = tcc-pass1; };
-  bash = callPackage ./bash-2.05b { tcc = tcc-pass1; };
+  tcc-mes = callPackage ./tinycc/0.9.27-pass1.nix { CC = "${final."tcc-0.9.26"}/bin/tcc"; };
+  libgetopt = callPackage ./libgetopt { tcc = tcc-mes; };
+  make = callPackage ./make-3.82 { tcc = tcc-mes; };
+  gzip = callPackage ./gzip-1.2.4 { tcc = tcc-mes; };
+  tar = callPackage ./tar-1.12 { tcc = tcc-mes; };
+  bzip2 = callPackage ./bzip2-1.0.8 { tcc = tcc-mes; };
+  sed = callPackage ./sed-4.0.9 { tcc = tcc-mes; };
+  patch = callPackage ./patch-2.5.9 { tcc = tcc-mes; };
+  coreutils = callPackage ./coreutils-5.0 { tcc = tcc-mes; };
+  heirloom-devtools = callPackage ./heirloom-devtools-070527 { tcc = tcc-mes; };
+  bash = callPackage ./bash-2.05b { tcc = tcc-mes; };
 
-  stage1env = callPackage ./stage1env { tcc = tcc-pass1; };
+  stage1env = callPackage ./stage1env { tcc = tcc-mes; };
   tcc-unwrapped = callPackage ./tinycc/0.9.27.nix { CC = "${final."tcc-0.9.26"}/bin/tcc"; };
   tcc-musl0 = callPackage ./tinycc/wrapped.nix
     rec
